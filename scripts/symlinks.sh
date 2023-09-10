ca5e9f32555d64c36d461d28ad16b447fe26b945
Caio Almeida <caioreix@MacBook-Pro-de-Anna.local>
2023-09-10 10:29:11 -0300

    temp

--- scripts/symlinks.sh
+++ scripts/symlinks.sh
@@ -1,15 +1,22 @@
 replace() {
-    files=$(find $1/$2 -maxdepth 1 -type f -name ".*")
+    files=$(find $1/$2 -maxdepth 1 -type f -name ".*" ! -name "*.bak")
     for file in $files; do
         name=$(basename $file)
         mv -f "$HOME/$name" "$1/old/$name-$(date +%s%N)"
         ln -s $1/$2/$name $HOME/$name
-        source $HOME/$name
+
+        if [ "$name" = ".zshrc" ]; then
+            if ! grep -q "DOTFILES=" "$HOME/$name"; then
+                echo "\nDOTFILES='$1'\n" >>"$HOME/$name"
+            else
+                sed -i'.bak' -e "s|DOTFILES=.*|DOTFILES='$1'|" "$1/$2/$name"
+            fi
+        fi
     done
 }
 
 rsource() {
-    files=$(find $1/$2 -maxdepth 1 -type f -name ".*")
+    files=$(find $1/$2 -maxdepth 1 -type f -name ".*" ! -name "*.bak")
     for file in $files; do
         name=$(basename $file)
         source $1/$2/$name
